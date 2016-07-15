cacba8574bf6afbeb5294e241573b31244ae2f06
Gedik <sgedik@ebay.com>
2016-07-15 16:42:37 +0300

    Added ratio

--- src/main/scala-2.11/RatioDataGenerator.scala
+++ src/main/scala-2.11/RatioDataGenerator.scala
@@ -1,4 +1,5 @@
 import scala.collection.immutable.HashMap
+import scala.collection.mutable
 import scala.util.Random
 
 /**
@@ -9,42 +10,77 @@ object RatioDataGenerator {
 
   def main(args: Array[String]): Unit = {
 
-    val itemRatio = HashMap("kola" -> 0.4,
- "süt" -> 0.222,
- "yogurt" -> 0.3,
- "bira" -> 0.2,
- "bez" -> 0.222,
- "kalem" -> 0.5,
- "iphone telefon" -> 0.8,
- "ipad" -> 0.666,
- "macbook" -> 0.67,
- "gitar" -> 0.44,
- "ekmek" -> 0.44,
- "yumurta" -> 0.223,
- "cornflakes" -> 0.12121212 ,
- "cikolata" -> 0.323232,
- "samsung telefon" -> 0.87654656456,
- "tv" -> 0.5656565656 ->
- "kahve" -> 0.4567654,
- "pizza" ->  0.23232,
- "lg telefon" -> 0.7875432,
- "lg kılıf" -> 0.7654343
+    val itemRatio = HashMap[String, Double]("kola" -> 0.4,
+      "süt" -> 0.222,
+      "yogurt" -> 0.3,
+      "bira" -> 0.2,
+      "bez" -> 0.222,
+      "kalem" -> 0.5,
+      "iphone telefon" -> 0.8,
+      "ipad" -> 0.666,
+      "macbook" -> 0.67,
+      "gitar" -> 0.44,
+      "ekmek" -> 0.44,
+      "yumurta" -> 0.223,
+      "cornflakes" -> 0.12121212,
+      "cikolata" -> 0.323232,
+      "samsung telefon" -> 0.87654656456,
+      "tv" -> 0.5656565656,
+      "kahve" -> 0.4567654,
+      "pizza" -> 0.23232,
+      "lg telefon" -> 0.7875432,
+      "lg kılıf" -> 0.7654343
     )
-  val random = new Random
 
-    for(i <- 1 to 10) {
+    val itemsByRemain = mutable.HashMap()
+    val maxTransactionToGenerate = 100000
+    val rowItemSize = Array[Int](maxTransactionToGenerate)
+    val itemsAsArray = itemRatio.toArray
+    val total = itemRatio.values.sum
+    val random = new Random
+    var counter =0
+    // println(itemsAsArray)
 
-      val maxPerLine = random.nextInt(itemRatio.size)
 
-      for(j <- 1 to maxPerLine) {
+    for (i <- 0 until maxTransactionToGenerate) {
+      val item = 1 + random.nextInt(itemRatio.size-1)
+      rowItemSize(i) = item
+      counter += item
+    }
+    val creditPerItem: Double = maxTransactionToGenerate*counter / total
+    var itemsByCredit = mutable.Map[String, Int]()
+    for (k <- itemRatio.keys) {
+      itemsByCredit += k -> (creditPerItem * itemRatio(k)).toInt
+    }
+    for (k <- itemsByCredit.keys)
+      println(k + " : " + itemsByCredit(k))
 
-      val maxPer
 
+    for (i <- 1 to 100000) {
+      var str = new StringBuilder
+
+      val maxPerLine = 1 + random.nextInt(itemRatio.size - 1)
+      var inserted: mutable.HashSet[Int] = new mutable.HashSet
+      for (j <- 1 to maxPerLine) {
+        var id = random.nextInt(itemRatio.size)
+        while (inserted.contains(id)) {
+          id = random.nextInt(itemRatio.size)
+        }
+        inserted += id
+        val item = itemsAsArray(id)._1
+        str ++= item
+        if (j != maxPerLine)
+          str += ','
+        else
+          str += '\n'
       }
+      print(str)
 
     }
 
-  //  itemRatio.filter(k => k._2> 0.5).toArray.
+
+
+
 
 
   }
